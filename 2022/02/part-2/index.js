5a909fc698aa95cf2c79d4006b517b386f2510b5
Romain Marbaix <befire@hotmail.fr>
2022-12-08 21:59:37 +0100

    2022-02: part 2

--- 2022/02/part-2/index.js
+++ 2022/02/part-2/index.js
@@ -4,25 +4,33 @@ const input = Fs.readFileSync('input.txt', { encoding: 'utf-8' });
 const rows = input.split('\r\n');
 
 const decrypt = {
-  'A':'Rock',
-  'B':'Paper',
-  'C':'Scissors',
-  'X':'Rock',
-  'Y':'Paper',
-  'Z':'Scissors',
+  'A': {opponent: 'Rock', expectedWinning: 'Paper', expectedLoss: 'Scissors'},
+  'B': {opponent: 'Paper', expectedWinning: 'Scissors', expectedLoss: 'Rock'},
+  'C': {opponent: 'Scissors', expectedWinning: 'Rock', expectedLoss: 'Paper'},
+};
+
+const resultMapping = {
+  'X': 'Lost',
+  'Y': 'Draw',
+  'Z': 'Won',
 };
 
 const scorePerMatch = rows.map((match)=> {
   let score = 0;
   const parts = match.trim().split(' ');
-  const opponentPlay = decrypt[parts[0]];
-  const myPlay = decrypt[parts[1]];
+  const decrypted = decrypt[parts[0]];
+  const expectedResult = resultMapping[parts[1]];
 
+  let myPlay = '';
+  if (expectedResult === 'Lost') myPlay = decrypted.expectedLoss;
+  if (expectedResult === 'Draw') myPlay = decrypted.opponent;
+  if (expectedResult === 'Won') myPlay = decrypted.expectedWinning;
+  
   if (myPlay === 'Rock') score += 1;
   if (myPlay === 'Paper') score += 2;
   if (myPlay === 'Scissors') score += 3;
-
-  const winner = gameResult(myPlay, opponentPlay);
+  
+  const winner = gameResult(myPlay, decrypted.opponent);
 
   if (winner === 'Lost') score += 0;
   if (winner === 'Draw') score += 3;
